b7f0c86124e58a818f937b5f278278679810c25a
rreyes <rreyes@ips-mexico.com>
2023-08-23 19:26:42 -0600

    deploy

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('vue3-js-b5'),
   routes: [
     {
       path: '/',
